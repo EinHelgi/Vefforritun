d3e1b94bd524c29eeecff477dbe48744085cb28b
EinHelgi <einhelgi@gmail.com>
2014-11-28 20:00:58 +0000

    eitthvað

--- getHighscore.php
+++ getHighscore.php
@@ -6,7 +6,7 @@ header('Content-Type: application/json');
 
 ini_set('display_errors', 'on');
 
-//$witch = $_GET['witch'];
+$witch = $_GET['witch'];
 
 try {
 	/**************************************
@@ -17,10 +17,10 @@ try {
 	// Create (connect to) SQLite database in file
 	$file_db = new SQLite3('standardScore.db');
 
-	//if($witch[0]===0) $query = $file_db->prepare("SELECT * FROM myllaScore ORDER BY score DESC LIMIT 10;");
-	//if($witch[0]===1) $query = $file_db->prepare("SELECT * FROM pacmanScore ORDER BY score DESC LIMIT 10;");
-	//if($witch[0]===2) $query = $file_db->prepare("SELECT * FROM breakoutScore ORDER BY score DESC LIMIT 10;");
-	$query = $file_db->prepare("SELECT * FROM myllaScore ORDER BY score DESC LIMIT 10;");
+	if($witch[0]=='0') $query = $file_db->prepare("SELECT * FROM myllaScore ORDER BY score DESC LIMIT 5;");
+	if($witch[0]=='1') $query = $file_db->prepare("SELECT * FROM pacmanScore ORDER BY score DESC LIMIT 10;");
+	if($witch[0]=='2') $query = $file_db->prepare("SELECT * FROM breakoutScore ORDER BY score DESC LIMIT 10;");
+	//$query = $file_db->prepare("SELECT * FROM myllaScore ORDER BY score DESC LIMIT 10;");
 
 	$result = $query->execute();
 
--- highscore.js
+++ highscore.js
@@ -2,18 +2,19 @@ var highScore = 0;
 var cach = ['myllaHighscore', 'pacmanHighscore', 'breakHighscore']
 
 function updateHighscore() {
-    var data = JSON.parse(localStorage.getItem(cach[highScore]));
+    $.get("getHighscore.php", {'witch[]':1}, function(data) {addToHtml(data);}, 'json');
+    /*if(highScore==0) data = JSON.parse(localStorage.getItem('myllaHighscore'));
     if(data===null) {
-        $.get("getHighscore.php", function(data) {addToHtml(data);}, 'json');
-        //, {'witch':highScore}
+        $.get("getHighscore.php", {'witch[]':highScore}, function(data) {addToHtml(data);}, 'json');
+        console.log(highScore);
     }
     else {
         addToHtml(data);
-    }
+    }*/
 }
 
 function addToHtml(data) {
-    localStorage.setItem(cach[highScore], JSON.stringify(data));
+    //localStorage.setItem('myllaHighscore', JSON.stringify(data));
     $('.highplayer').empty();
     $('.highScore').empty();
     for (var i=0;i<data.length;++i) {
@@ -25,7 +26,7 @@ function addToHtml(data) {
 
 function insertToHighscore(data){
 
-    var oldScore = JSON.parse(localStorage.getItem(cach[highScore]));
+    var oldScore = JSON.parse(localStorage.getItem('myllaHighscore'));
     var done = false;
     var newScore = [];
     for (var i=0; i<20; ++i) {
@@ -33,6 +34,7 @@ function insertToHighscore(data){
             newScore.push(data[0]);
             newScore.push(data[1]);
             done = true;
+            console.log('i am here');
         }
         newScore.push(oldScore[i]);
         ++i;
@@ -40,5 +42,5 @@ function insertToHighscore(data){
     }
     newScore.splice(20);
 
-    localStorage.setItem(cach[highScore], JSON.stringify(newScore));
+    localStorage.setItem('myllaHighscore', JSON.stringify(newScore));
 }
